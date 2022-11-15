8c7b665121dff82e64ecc7940874f1a89d2eaad1
Asep Saepudin <51154597+seapudinrpl1@users.noreply.github.com>
2022-11-16 06:18:57 +0700

    Tugas JWD Ke 2

---
+++ Tugas2_AsepSaepudin.php
@@ -0,0 +1,45 @@
+<?php
+
+
+// Versi Pertama
+// Start Echo
+echo "
+    <!DOCTYPE html>
+    <html lang='en'>
+    <head>
+        <meta charset='UTF-8'>
+        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <title>Mengeksekusi Source Code</title>
+    </head>
+    <body>
+        <p>
+            Selamat Datang <br>
+            Ayo kita belajar Junior Web Developer bersama <br>
+            di BPPTIK Cikarang
+        </p>
+    </body>
+    </html>
+";
+// End Echo
+
+
+?>
+
+<!-- Versi Kedua -->
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mengeksekusi Source Code</title>
+</head>
+<body>
+    <?php
+        // echo "<p>Selamat Datang <br>Ayo kita belajar Junior Web Developer bersama <br>di BPPTIK Cikarang</p>";
+    ?>
+    
+</body>
+</html> -->
